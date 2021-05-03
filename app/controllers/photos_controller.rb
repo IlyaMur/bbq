b6b55b26402f7f4d6f16fbf2d852c30d0818a727
Ilya Mur <ilyamur@hotmail.com>
2021-05-03 17:03:52 +0300

    fix typo in email

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -41,6 +41,7 @@ class PhotosController < ApplicationController
 
   def notify_subscibers(event, photo)
     all_emails = event.subscriptions.map(&:user_email) - [photo.user]
+  
 
     all_emails.each { |email| EventMailer.photo(event, email, photo).deliver_now }
   end
