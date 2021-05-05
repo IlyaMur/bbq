1ac80765abe1f1789906169fcb927a30a108130e
Ilya Mur <ilyamur@hotmail.com>
2021-05-05 17:21:08 +0300

    fix yandex maps, add lightbox for events photos

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,11 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
 import "@fortawesome/fontawesome-free/css/all"
-
+import "ekko-lightbox"
+import './lightbox'
+import './map'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
