6578cdebabc02b993fa9147b1860cf2475008064
Ilya Mur <ilyamur@hotmail.com>
2021-05-05 20:25:07 +0300

    remove turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import './map'
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
 import "@fortawesome/fontawesome-free/css/all"
 import "ekko-lightbox"
 import './lightbox'
+import './map'
 
 Rails.start()
 Turbolinks.start()
