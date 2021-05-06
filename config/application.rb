31b2408c9adfeb3a82dca0c1f1826184a03f8607
Ilya Mur <ilyamur@hotmail.com>
2021-05-07 00:16:59 +0300

    third fix for heroku

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module BbqForMyFriends
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
     config.load_defaults 6.1
     config.i18n.default_locale = :ru
 
