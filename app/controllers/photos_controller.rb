a95b66094838827f20b258ba69bc7e18ccd34fd2
Ilya Mur <ilyamur@hotmail.com>
2021-05-08 00:44:36 +0300

    fix photos for private events

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,11 @@ class PhotosController < ApplicationController
   def create
     @new_photo = @event.photos.build(photo_params)
     @new_photo.user = current_user
-    if @new_photo.save
+
+    if @new_photo.valid?
+      redirect_to root_path, alert: I18n.t('controllers.photos.error') and return unless pincode_valid?(@event)
+
+      @new_photo.save
       notify_subscibers(@event, @new_photo)
       redirect_to @event, notice: I18n.t('controllers.photos.created')
     else
