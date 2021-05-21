9dccc01d2bf37b09df8c05692b6a1353172f0e16
Ilya Mur <ilyamur@hotmail.com>
2021-05-21 15:11:15 +0300

    fix devise mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+  
   private
 
   def set_name
@@ -21,7 +25,4 @@ class User < ApplicationRecord
     Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
   end
 
-  def send_devise_notification(notification, *args)
-    devise_mailer.send(notification, self, *args).deliver_later
-  end
 end
