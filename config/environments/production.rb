7684000a9aae83428a9f73612775f315226e087d
Ilya Mur <ilyamur@hotmail.com>
2021-05-21 14:58:31 +0300

    add jobs for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,9 +100,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://warm-meetings.ru/' }
+  config.action_mailer.default_url_options = { host: 'warm-meetings.ru' }
   config.action_mailer.delivery_method = :mailjet
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
