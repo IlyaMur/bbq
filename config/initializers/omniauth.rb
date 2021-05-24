6fad0ec232e9f0b563fb73a0301a5dac9e7f12cf
Ilya Mur <ilyamur@hotmail.com>
2021-05-25 02:46:23 +0300

    remove redundant files

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, Rails.application.credentials.dig(:development, :omni, :vk_id),
-  Rails.application.credentials.dig(:development, :omni, :vk_key)
-end
